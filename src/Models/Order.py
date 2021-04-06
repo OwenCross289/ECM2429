401ff9a1228fb8adbfe5a8fb36d51aedc904b51d
Owen <oc289@exeter.ac.uk>
2021-04-06 22:29:15 +0100

    Added doc strings to order

--- src/Models/Order.py
+++ src/Models/Order.py
@@ -4,7 +4,17 @@ import os
 
 class Order:
 
-    def __init__(self, order_id: int, customer_name: str, customer_address: str, post_code: str, email: str, item: Item, order_status):
+    def __init__(self, order_id: int, customer_name: str, customer_address: str, post_code: str, email: str, item: Item, order_status: str):
+        '''It will be called when you click on the file name
+        or the catalog selection button.
+        :param order_id: int: ID of Order in database
+        :param customer_name: str: Name of customer who placed order
+        :param customer_address: str: Address of customer who placed order
+        :param post_code: int: Post code of customer who placed order
+        :param email: str: email of customer who placed order
+        :param item: Item: Item the customer has ordered
+        :param order_status: str: Staus of order, should only be 'Ready' or 'Shipping'
+        '''
         self.order_id = order_id
         self.customer_name = customer_name
         self.customer_address = customer_address
@@ -13,7 +23,11 @@ class Order:
         self.item = item
         self.order_status = order_status
 
-    def print_pdf(self, path):
+    def print_pdf(self, path: str):
+        '''Prints a shipping label, currently writes to disk
+        as prototype
+        :param path: str: Folder location to write pdf
+        '''
         #As this is just a prototype this just creates a PDF in specified location.
         document_title = "ECM2429 Store"
         file_name = os.path.join(path, f"{self.customer_name}{self.order_id}.pdf")
